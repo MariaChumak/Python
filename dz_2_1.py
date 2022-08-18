1e2d867c93a379f224ec8ee3363844d1f2cafe53
MariaChumak <95484134+MariaChumak@users.noreply.github.com>
2022-08-18 23:06:04 +0300

    Add files via upload

---
+++ dz_2_1.py
@@ -0,0 +1,9 @@
+expression_1 = 15 * 3
+expression_2 = 15 / 3
+expression_3 = 15 // 2
+expression_4 = 15 ** 2
+
+print(type(expression_1))
+print(type(expression_2))
+print(type(expression_3))
+print(type(expression_4))
